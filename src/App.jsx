7e5fd43b3913a0d6c50a2f639ce3e428e0ed907f
hxp <gp3098@live.cn>
2022-10-02 00:20:14 +0800

    add first demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import { useRef } from 'react';
+import { useEffect } from 'react';
+import { fromEvent } from 'rxjs';
+//针对这个公众号的文章进行学习
+//https://mp.weixin.qq.com/s/5sXPioGOZ73narerYZF9Wg
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const btnRef = useRef(null);
+  useEffect(() => {
+    const btnClick$ = fromEvent(btnRef.current, 'click');
+    const subscription = btnClick$.subscribe((e) => {
+      console.log(e);
+    });
+    return () => subscription.unsubscribe();
+  });
   return (
     <div className="App">
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button ref={btnRef}>Click me</button>
       </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </div>
   );
 }
