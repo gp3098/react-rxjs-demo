796c07884918c081feb192264fb19ddbb9dce1ec
hxp <gp3098@live.cn>
2022-10-02 14:27:54 +0800

    Implement your own double click button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { useRef } from 'react';
 import { useEffect } from 'react';
-import { fromEvent } from 'rxjs';
+import { asyncScheduler, buffer, filter, fromEvent, map, throttleTime } from 'rxjs';
 //针对这个公众号的文章进行学习
 //https://mp.weixin.qq.com/s/5sXPioGOZ73narerYZF9Wg
 
@@ -9,7 +9,17 @@ function App() {
   const btnRef = useRef(null);
   useEffect(() => {
     const btnClick$ = fromEvent(btnRef.current, 'click');
-    const subscription = btnClick$.subscribe((e) => {
+
+    const throttled$ = btnClick$.pipe(throttleTime(250, asyncScheduler, { leading: false, trailing: true }));
+
+    const btnDblClick$ = btnClick$.pipe(
+      buffer(throttled$),
+      map((arr) => arr.length),
+      filter((len) => len === 2)
+    );
+
+    const subscription = btnDblClick$.subscribe((e) => {
+      console.log('双击啦');
       console.log(e);
     });
     return () => subscription.unsubscribe();
